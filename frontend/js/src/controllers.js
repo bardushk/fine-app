6f9eb88eb9fd31a6836e6e5e173ab46496286e63
Nickolay Bardushko <nb@shtrafy-gibdd.ru>
2015-10-30 17:07:29 +0300

    Добавлены комментарии.

--- frontend/js/src/controllers.js
+++ frontend/js/src/controllers.js
@@ -2,8 +2,18 @@
  * Модуль с контроллерами.
  */
 angular.module('controllers', ['validator-service'])
-    .controller('FinesController', function($scope, $http, ValidatorService) {
+    .controller('FinesController', function ($scope, $http, ValidatorService) {
+
+        /**
+         * Список автомобилей.
+         * @type {{}}
+         */
         $scope.autos = {};
+
+        /**
+         * Валидатор.
+         * @type {ValidatorService.validateAutoNumber|Function}
+         */
         $scope.isValid = ValidatorService.validateAutoNumber;
 
         /**
@@ -12,7 +22,7 @@ angular.module('controllers', ['validator-service'])
          * @param autos array - массив автомобилей
          * @param autoNumber string - номер автомобиля
          */
-        $scope.remove = function(autos, autoNumber) {
+        $scope.remove = function (autos, autoNumber) {
             delete autos[autoNumber];
         };
 
@@ -22,11 +32,11 @@ angular.module('controllers', ['validator-service'])
          * @param autos array - массив автомобилей
          * @param autoNumber string - номер автомобиля
          */
-        $scope.check = function(autos, autoNumber) {
+        $scope.check = function (autos, autoNumber) {
             if (!ValidatorService.validateAutoNumber(autoNumber)) {
                 return;
             }
-            return $http.get('/api', {params:  {auto_number: autoNumber}}).then(function(response){
+            return $http.get('/api', {params: {auto_number: autoNumber}}).then(function (response) {
                 autos[autoNumber] = response.data;
             });
         }
